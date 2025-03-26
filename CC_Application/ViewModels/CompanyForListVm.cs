0a54f97f995affaa50a2910403be6cf3a22fcf84
Rdzanu <grzegordzanek@gmail.com>
2025-03-26 17:45:07 +0100

    Add first endpoint and minor tweaks

--- CC_Application/ViewModels/CompanyForListVm.cs
+++ CC_Application/ViewModels/CompanyForListVm.cs
@@ -5,5 +5,5 @@ public class CompanyForListVm
     public int Id { get; set; }
     public string CompanyName { get; set; }
     public string Groups { get; set; }
-    public string NextRaportDate { get; set; }
+    public DateTime NextRaportDate { get; set; }
 }
